e94cb7c21a5c13b3342568315a86ba80fe61dcae
RaynaRaven <carolmarjara@gmail.com>
2023-10-22 00:47:42 +0100

    update colors & themes

--- app/src/main/java/org/setu/focussphere/TaskActivity.kt
+++ app/src/main/java/org/setu/focussphere/TaskActivity.kt
@@ -3,17 +3,22 @@ package org.setu.focussphere
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import org.setu.focussphere.databinding.ActivityMainBinding
+import org.setu.focussphere.databinding.ActivityTaskBinding
+import timber.log.Timber
+import timber.log.Timber.Forest.i
 
-class MainActivity : AppCompatActivity() {
+class TaskActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityTaskBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Timber.plant(Timber.DebugTree())
+        i("task activity started")
 
         binding.addTaskButton.setOnClickListener {
             val taskAddedText = getString(R.string.task_add_text)
