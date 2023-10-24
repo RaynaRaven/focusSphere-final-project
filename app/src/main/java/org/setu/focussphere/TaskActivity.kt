19447ce4012c9e789d4ea024062bd88bb6a915fb
RaynaRaven <carolmarjara@gmail.com>
2023-10-24 23:01:11 +0100

    persist tasks to arraylist

--- app/src/main/java/org/setu/focussphere/TaskActivity.kt
+++ app/src/main/java/org/setu/focussphere/TaskActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import org.setu.focussphere.databinding.ActivityTaskBinding
 import org.setu.focussphere.models.PriorityLevel
+import org.setu.focussphere.models.TaskModel
 import org.setu.focussphere.models.TaskStatus
 import timber.log.Timber
 import timber.log.Timber.Forest.i
@@ -15,13 +16,13 @@ import timber.log.Timber.Forest.i
 class TaskActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTaskBinding
+    var task = TaskModel()
+    val tasks = ArrayList<TaskModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         Timber.plant(Timber.DebugTree())
         i("task activity started")
 
@@ -33,16 +34,31 @@ class TaskActivity : AppCompatActivity() {
         statusAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.statusSpinner.adapter = statusAdapter
 
-        binding.addTaskButton.setOnClickListener {
-            val taskTitle = binding.taskTitle.text.toString()
-            val taskDescription = binding.taskDescription.text.toString()
-
+        binding.addTaskButton.setOnClickListener() {
+            i("Button onClick triggered")
             val selectedPriority = binding.prioritySpinner.selectedItem as PriorityLevel
             val selectedStatus = binding.statusSpinner.selectedItem as TaskStatus
 
-            val taskAddedText = getString(R.string.task_add_text)
-            Snackbar.make(it, taskAddedText, Snackbar.LENGTH_SHORT).show()
-//            Toast.makeText(this, taskAddedText, Toast.LENGTH_SHORT).show()
+            task.title = binding.taskTitle.text.toString()
+            task.description = binding.taskDescription.text.toString()
+            task.priorityLevel = selectedPriority
+            task.status = selectedStatus
+
+            if (task.title.isNotBlank() && !tasks.contains(task)) {
+                tasks.add(task.copy())
+                binding.taskTitle.text.clear()
+                binding.taskDescription.text.clear()
+                i("Button Pressed: $task")
+                Snackbar.make(it,(	"\ud83e\udd70") + "  " + getString(R.string.task_add_text), Snackbar.LENGTH_SHORT).show()
+                for (i in tasks.indices)
+                    { i("Task[$i]:${this.tasks[i]}") }
+
+            } else if (task.title.isBlank()){
+                Snackbar.make(it, "\ud83d\ude31" + "  Task must contain a title", Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(it, "\ud83d\ude31" + "  duplicate task not added", Snackbar.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
