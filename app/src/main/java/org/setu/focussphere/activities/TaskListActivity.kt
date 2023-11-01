3323e34501643aea68faebf4490152e5d2a013fc
RaynaRaven <carolmarjara@gmail.com>
2023-11-01 15:43:28 +0000

    adding menu button to actionbar

--- app/src/main/java/org/setu/focussphere/activities/TaskListActivity.kt
+++ app/src/main/java/org/setu/focussphere/activities/TaskListActivity.kt
@@ -3,6 +3,7 @@ package org.setu.focussphere.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,12 +22,19 @@ class TaskListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTaskListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.toolbar.title = title
+        setSupportActionBar(binding.toolbar)
         app = application as MainApp
 
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = TaskAdapter(app.tasks)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
 
