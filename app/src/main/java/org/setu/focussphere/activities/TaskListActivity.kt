93dcdf0449f8698d1190f1dda0e82d03e2546514
RaynaRaven <carolmarjara@gmail.com>
2023-11-02 10:33:07 +0000

    refactor adapter to own class file

--- app/src/main/java/org/setu/focussphere/activities/TaskListActivity.kt
+++ app/src/main/java/org/setu/focussphere/activities/TaskListActivity.kt
@@ -62,30 +62,3 @@ class TaskListActivity : AppCompatActivity() {
         }
 }
 
-class TaskAdapter constructor(private var tasks: List<TaskModel>) :
-    RecyclerView.Adapter<TaskAdapter.MainHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        val binding = CardTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-
-        return MainHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val task = tasks[holder.adapterPosition]
-        holder.bind(task)
-    }
-
-    override fun getItemCount(): Int = tasks.size
-
-    class MainHolder(private val binding : CardTaskBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(task: TaskModel) {
-            binding.taskTitle.text = task.title
-            binding.description.text = task.description
-            binding.priority.text = task.priorityLevel.toString()
-            binding.status.text = task.status.toString()
-        }
-    }
-}
