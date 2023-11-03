5c849aa475ab018778656c3b968b7f7a4badc542
RaynaRaven <carolmarjara@gmail.com>
2023-11-03 19:19:13 +0000

    adding update task functionality

--- app/src/main/java/org/setu/focussphere/activities/TaskActivity.kt
+++ app/src/main/java/org/setu/focussphere/activities/TaskActivity.kt
@@ -35,7 +35,7 @@ class TaskActivity : AppCompatActivity() {
         app = application as MainApp
         i("Task Activity Started")
 
-
+        var edit = false
 
         val priorityAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, PriorityLevel.values())
         priorityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -46,14 +46,16 @@ class TaskActivity : AppCompatActivity() {
         binding.statusSpinner.adapter = statusAdapter
 
         if (intent.hasExtra("task_edit")) {
+            edit = true
             task = intent.extras?.getParcelable("task_edit")!!
             binding.taskTitle.setText(task.title)
             binding.taskDescription.setText(task.description)
             binding.statusSpinner.setSelection(task.status.ordinal)
             binding.prioritySpinner.setSelection(task.priorityLevel.ordinal)
+            binding.addTaskButton.setText(R.string.save_task)
         }
 
-        binding.addTaskButton.setOnClickListener() {
+        binding.addTaskButton.setOnClickListener() { it ->
             i("Button onClick triggered")
             val selectedPriority = binding.prioritySpinner.selectedItem as PriorityLevel
             val selectedStatus = binding.statusSpinner.selectedItem as TaskStatus
@@ -63,21 +65,29 @@ class TaskActivity : AppCompatActivity() {
             task.priorityLevel = selectedPriority
             task.status = selectedStatus
 
-            if (task.title.isNotBlank() && !app.tasks.findAll().contains(task)) {
-                app.tasks.create(task.copy())
-                binding.taskTitle.text.clear()
-                binding.taskDescription.text.clear()
-                i("Button Pressed: $task")
-                Snackbar.make(it,(	"\ud83e\udd70") + "  " + getString(R.string.task_add_text), Snackbar.LENGTH_SHORT).show()
-                setResult(RESULT_OK)
-                finish()
+            if (task.title.isBlank()){
+                Snackbar.make(it, R.string.snackbar_warning_missingTitle, Snackbar.LENGTH_LONG).show()
             }
-            else if (task.title.isBlank()){
-                Snackbar.make(it, getString(R.string.snackbar_warning_missingTitle), Snackbar.LENGTH_LONG).show()
+            else if (!edit && app.tasks.findAll().any { it.title == task.title && it.description == task.description }) {
+                Snackbar.make(it, R.string.snackbar_warning_duplicateTask, Snackbar.LENGTH_SHORT).show()
             }
             else {
-                Snackbar.make(it, getString(R.string.snackbar_warning_duplicateTask), Snackbar.LENGTH_SHORT).show()
+                if (edit) {
+                    app.tasks.update(task.copy())
+//                    setResult(RESULT_OK)
+//                    finish()
+                } else {
+                    app.tasks.create(task.copy())
+                    binding.taskTitle.text.clear()
+                    binding.taskDescription.text.clear()
+                    i("Button Pressed: $task")
+                    Snackbar.make(it, "\ud83e\udd70" + R.string.task_add_text, Snackbar.LENGTH_SHORT).show()
+//                    setResult(RESULT_OK)
+//                    finish()
+                }
             }
+            setResult(RESULT_OK)
+            finish()
         }
     }
 
