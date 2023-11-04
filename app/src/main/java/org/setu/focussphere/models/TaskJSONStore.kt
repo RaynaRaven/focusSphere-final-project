8a4a11309d511c1b82e4aa7d35987100837dd7ac
RaynaRaven <carolmarjara@gmail.com>
2023-11-04 21:12:52 +0000

    adding read write helpers and JSON store model

---
+++ app/src/main/java/org/setu/focussphere/models/TaskJSONStore.kt
@@ -0,0 +1,84 @@
+package org.setu.focussphere.models
+
+import android.content.Context
+import android.net.Uri
+import com.google.android.gms.tasks.Tasks
+import com.google.gson.*
+import com.google.gson.reflect.TypeToken
+import org.setu.focussphere.helpers.*
+import timber.log.Timber
+import java.lang.reflect.Type
+import java.util.*
+
+const val JSON_FILE = "placemarks.json"
+val gsonBuilder: Gson = GsonBuilder().setPrettyPrinting()
+    .registerTypeAdapter(Uri::class.java, UriParser())
+    .create()
+val listType: Type = object : TypeToken<ArrayList<TaskModel>>() {}.type
+
+fun generateRandomId(): Long {
+    return Random().nextLong()
+}
+
+class TaskJSONStore(private val context: Context) : TaskStore {
+
+    var tasks = mutableListOf<TaskModel>()
+
+    init {
+        if (exists(context, JSON_FILE)) {
+            deserialize()
+        }
+    }
+
+    override fun findAll(): MutableList<TaskModel> {
+        logAll()
+        return tasks
+    }
+
+    override fun create(task: TaskModel) {
+        task.id = generateRandomId()
+        tasks.add(task)
+        serialize()
+    }
+
+
+    override fun update(task: TaskModel) {
+        // todo
+    }
+
+    override fun delete(task: TaskModel) {
+        TODO("Not yet implemented")
+    }
+
+    private fun serialize() {
+        val jsonString = gsonBuilder.toJson(tasks, listType)
+        write(context, JSON_FILE, jsonString)
+    }
+
+    private fun deserialize() {
+        val jsonString = read(context, JSON_FILE)
+        tasks = gsonBuilder.fromJson(jsonString, listType)
+    }
+
+    private fun logAll() {
+        tasks.forEach { Timber.i("$it") }
+    }
+}
+
+class UriParser : JsonDeserializer<Uri>,JsonSerializer<Uri> {
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): Uri {
+        return Uri.parse(json?.asString)
+    }
+
+    override fun serialize(
+        src: Uri?,
+        typeOfSrc: Type?,
+        context: JsonSerializationContext?
+    ): JsonElement {
+        return JsonPrimitive(src.toString())
+    }
+}
