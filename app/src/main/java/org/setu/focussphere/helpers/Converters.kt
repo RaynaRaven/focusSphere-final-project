e72aade4c69d28d553fcbf899936b0228bed92ee
RaynaRaven <carolmarjara@gmail.com>
2024-03-05 22:46:07 +0000

    Adding TypeConverter functions for task priority level

--- app/src/main/java/org/setu/focussphere/helpers/Converters.kt
+++ app/src/main/java/org/setu/focussphere/helpers/Converters.kt
@@ -1,23 +1,35 @@
 package org.setu.focussphere.helpers
 
 import androidx.room.TypeConverter
+import org.setu.focussphere.data.enums.PriorityLevel
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 
 class Converters {
 
-    //room type converter for LocalDateTime Object to Long
+    //Room type converter for LocalDateTime Object to Long
     @TypeConverter
     fun fromLocalDateTime(value: LocalDateTime?): Long? {
         return value?.atZone(ZoneId.systemDefault())?.toInstant()?.toEpochMilli()
     }
 
-    //room type converter for Long to LocalDateTime
+    //Room type converter for Long to LocalDateTime
     @TypeConverter
     fun fromLocalDateTime(value: Long?): LocalDateTime? {
         return value?.let {
             Instant.ofEpochMilli(it).atZone(ZoneId.systemDefault()).toLocalDateTime()
         }
     }
+    
+    @TypeConverter
+    fun fromPriorityLevel(priorityLevel: PriorityLevel): Int {
+        return priorityLevel.order
+    }
+
+    @TypeConverter
+    fun toPriorityLevel(order: Int): PriorityLevel {
+        return PriorityLevel.values().first { it.order == order }
+    }
+
 }
