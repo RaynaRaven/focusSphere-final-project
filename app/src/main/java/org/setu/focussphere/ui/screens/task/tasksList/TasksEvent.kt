4b49f34136fac62fafe030631b1577ffbca5872a
RaynaRaven <carolmarjara@gmail.com>
2024-03-06 23:32:43 +0000

    refactoring event class names

--- app/src/main/java/org/setu/focussphere/ui/screens/task/tasksList/TasksEvent.kt
+++ app/src/main/java/org/setu/focussphere/ui/screens/task/tasksList/TasksEvent.kt
@@ -4,8 +4,8 @@ import org.setu.focussphere.data.entities.Task
 
 sealed class TasksEvent {
     data class OnDeleteTaskClick(val task: Task) : TasksEvent()
-    data class onDoneChange(val task: Task, val isDone: Boolean) : TasksEvent()
+    data class OnDoneChange(val task: Task, val isDone: Boolean) : TasksEvent()
     object OnUndoDeleteClick : TasksEvent()
-    data class onTaskClick(val task: Task) : TasksEvent()
+    data class OnTaskClick(val task: Task) : TasksEvent()
     object OnAddTaskClick : TasksEvent()
 }
