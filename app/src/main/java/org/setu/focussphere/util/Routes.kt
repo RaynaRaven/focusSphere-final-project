c58de30bb5d5279a7f3234e8a284ae60bd068150
RaynaRaven <carolmarjara@gmail.com>
2024-03-13 01:16:59 +0000

    Adding navigation routes object

--- app/src/main/java/org/setu/focussphere/util/Routes.kt
+++ app/src/main/java/org/setu/focussphere/util/Routes.kt
@@ -4,5 +4,13 @@ object Routes {
     const val SPLASH = "splash"
     const val TASK_LIST = "task_list"
     const val ADD_EDIT_TASK = "add_edit_task"
+    const val TASK_DETAILS = "task_details"
+    const val ROUTINES = "routines"
+    const val ADD_EDIT_ROUTINE = "add_edit_routine"
+    const val DASHBOARD = "dashboard"
+    const val TASK_TRACKER = "task-tracker"
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+    const val HOME = "home"
 
 }
